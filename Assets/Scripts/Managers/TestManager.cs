0d7c31cc2d0cb7cd65f4ee3bea417a936930e2a5
Mkonneman <Mkonneman@ksu.edu>
2026-06-20 17:33:18 -0500

    Gritty on haters

--- Assets/Scripts/Managers/TestManager.cs
+++ Assets/Scripts/Managers/TestManager.cs
@@ -1,50 +1,60 @@
 using System.Collections.Generic;
 using UnityEngine;
- 
-// Temporary test script to prove the Order/Station pipeline works end to end.
-// Drag the three v1 MenuItem assets (burger, fries, drink) into the Inspector
-// slots below, drag the GrillStation into the grillStation slot, hit Play,
-// and watch the Console.
+
 public class TestManager : MonoBehaviour
 {
     [Header("Drag your three v1 MenuItem assets here")]
     public MenuItem burgerMenuItem;
     public MenuItem friesMenuItem;
     public MenuItem drinkMenuItem;
- 
+
     [Header("Drag the GrillStation GameObject here")]
     public Station grillStation;
- 
+
+    [Header("Drag the OrderTicketUI here")]
+    public OrderTicketUI orderTicketUI;
+
     private Order testOrder;
- 
+
     void Start()
     {
-        // Build a test order containing all three items - this is a "combo"
-        // simply by virtue of having more than one OrderItem in the list.
-        List<MenuItem> menuItems = new List<MenuItem> { burgerMenuItem, friesMenuItem, drinkMenuItem };
+        // Build a test order with all three items
+        List<MenuItem> menuItems = new List<MenuItem> 
+        { 
+            burgerMenuItem, 
+            friesMenuItem, 
+            drinkMenuItem 
+        };
         testOrder = new Order(menuItems);
- 
+
+        // Add some test customizations to the burger
+        testOrder.items[0].requestedCustomizations.Add("No Onions");
+        testOrder.items[0].requestedCustomizations.Add("Extra Cheese");
+
         Debug.Log($"Created test order with {testOrder.items.Count} items.");
- 
-        foreach (OrderItem item in testOrder.items)
+
+        // Display the order on the ticket UI
+        if (orderTicketUI != null)
         {
-            ProcessType? current = item.GetCurrentProcess();
-            Debug.Log($"{item.menuItem.itemName} currently needs: {current}");
+            orderTicketUI.DisplayOrder(testOrder);
         }
- 
-        // Try assigning every item that currently needs a process the
-        // Grill station supports (GrillSear or FryTimer).
+        else
+        {
+            Debug.LogWarning("OrderTicketUI not assigned in TestManager!");
+        }
+
+        // Try assigning items to the grill station
         foreach (OrderItem item in testOrder.items)
         {
             ProcessType? current = item.GetCurrentProcess();
             if (current.HasValue && grillStation.SupportsProcess(current.Value))
             {
                 bool assigned = grillStation.TryAssignItem(item);
-                Debug.Log($"Tried assigning {item.menuItem.itemName} ({current.Value}) to GrillStation: {assigned}");
+                Debug.Log($"Assigned {item.menuItem.itemName} ({current.Value}) to GrillStation: {assigned}");
             }
             else
             {
-                Debug.Log($"{item.menuItem.itemName} needs {current} which GrillStation does not support - skipping for this test.");
+                Debug.Log($"{item.menuItem.itemName} needs {current} - not handled by GrillStation.");
             }
         }
     }
